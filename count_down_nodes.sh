fd22ec4f5ea7f12c9e1aa8ce5361059ae3c10e11
tin6150 <tin@brcmst>
2021-05-05 17:37:47 -0700

    ancilliary script/notes that is does not need to be part of bofhbot

--- count_down_nodes.sh
+++ count_down_nodes.sh
@@ -1,9 +1,28 @@
+date
 
-echo 'sinfo --Node | egrep idle | grep savio | wc'
-sinfo --Node | egrep idle | grep savio | wc
+echo 'sinfo --Node | egrep idle | grep savio | wc -l'
+NumIdle=$(sinfo --Node | egrep idle | grep savio | wc -l)
 
-echo 'sinfo --Node | egrep idle\|drain | grep savio | wc'
-sinfo --Node | egrep idle\|drain | grep savio | wc
+
+echo 'sinfo --Node | egrep idle\|drain | grep savio | wc -l'
+NumIdleDrain=$(sinfo --Node | egrep idle\|drain | grep savio | wc -l)
+
+#echo 'sinfo --Node -R -S %E --format="%9u %19H %6t %N %E"| grep Pwr | wc -l'
+#NumPwr=$(sinfo --Node -R -S %E --format="%9u %19H %6t %N %E"| grep Pwr | wc -l)
+
+echo 'sinfo --Node -R -S %E --format="%9u %19H %6t %N %E"| egrep Pwr\|sctrl_reservation | wc -l'
+NumPwr=$(sinfo --Node -R -S %E --format="%9u %19H %6t %N %E"| egrep Pwr\|sctrl_reservation | wc -l)
 
 echo 'wwstats -s | egrep Dead\|utilization'
-wwstats -s | egrep Dead\|utilization
+#wwstats -s | egrep Dead\|utilization
+NumWwDead=$(wwstats -s | egrep Dead | awk -F: '{print $2}')
+NumWwUtil=$(wwstats -s | egrep utilization | awk -F: '{print $2}')
+
+#echo $NumIdle,$NumIdleDrain,$NumPwr,$NumWwDead,$NumWwUtil
+echo $NumIdle,$NumIdleDrain,$NumPwr,$NumWwDead,$NumWwUtil | sed 's/,/\t/g'
+
+
+####
+#### by user visible partition, not cluster name...
+#### scontrol show partitions | egrep PartitionName\|TotalNode
+
--- power_check.sh
+++ power_check.sh
@@ -16,15 +16,22 @@ pdsh -w n0[254-289].savio2,n0[295-297].savio2 ipmitool sensor |grep Watt |sort -
 pdsh -w n0[006-125].savio3  ipmitool sensor | grep Watt
 
 
+# savio2 : 36W - 234W 
+pdsh -w n0[000-034].savio2  ipmitool sensor | grep Watt
 
 
 # savio2_1080ti # SMC ? ... 195-435W
 #pdsh -w n0[298-301].savio2  ipmitool sensor | grep Power 
 pdsh -w n0[298-301].savio2  ipmitool sensor | grep Watt
 
-# savio2 : 36W - 234W 
-pdsh -w n0[000-034].savio2  ipmitool sensor | grep Watt
+# savio2_knl
+pdsh -w n0[254-281].savio2  ipmitool sensor | grep Watt
+
 
 
 # savio1, no Power number...  Amp 5-25, many ~10-17
 pdsh -w n0[004-071].savio1  ipmitool sensor | grep Amp
+
+
+# incomplete.  collected the data in spreadsheet (DC Map for Warren)
+# 
