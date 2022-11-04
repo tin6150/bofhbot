febaca51d1fea0bda251710a3514f9f2fc9ab263
tin6150 <tin@brcmst>
2022-11-04 16:37:27 -0700

    0.3 chkGpu explicit warning on gpuError

--- checkGpuOnNode.py
+++ checkGpuOnNode.py
@@ -8,11 +8,13 @@
 ## exp: 4 ## pdsh -w n0[134-138,158-161].savio3 /global/home/users/tin/tin-gh/bofhbot/checkGpuOnNode.py   
 ## exp: 2 ## pdsh -w n0[209-217,262-264].savio3 /global/home/users/tin/tin-gh/bofhbot/checkGpuOnNode.py   
 ## exp: 2 ## pdsh -w n0[004-005].savio3 /global/home/users/tin/tin-gh/bofhbot/checkGpuOnNode.py   
+## comb   ## pdsh -w n0[143-145,174-176,258-261].savio3,n0[134-138,158-161].savio3,n0[004-005,209-217,262-264].savio3,n0[012-026,223-226,298-302].savio2 /global/home/users/tin/tin-gh/bofhbot/checkGpuOnNode.py   
 
 ## version/changes
 ## 0.1  Tin usable deviceQuery detection 
 ## 0.1a Prepping for Hamza's PR
 ## 0.2  merged with Hamza's code
+## 0.3  Tin - better error detection, output
 
 
 import socket 
@@ -20,15 +22,15 @@ import os
 # bothbot_lib mostly for "import os" and the dbg fn
 import bofhbot_lib
 from bofhbot_lib import *
-# could be set by argparse -v, recycle from bofhbot.py ++FIXME++
-bofhbot_lib.verboseLevel  = 6 #6
-bofhbot_lib.dbgLevel      = 6 #6
+# TODO: use argparse -v, recycle from bofhbot.py ++FIXME++
+bofhbot_lib.verboseLevel  = 1 #6 = very verbose; 0 = silent (check exist code, syslog/email, eg use in cron) 
+bofhbot_lib.dbgLevel      = 1 #6
 
 # global param :)  better as OOP get() fn or some such.
 devQueryOutFile = f'/var/tmp/devQuery.{os.getlogin()}.out' # store deviceQuery output
 osDevOutFile = f'/var/tmp/osDev.{os.getlogin()}.out'       # store ls -l /dev/nvidia* 
 
-emailRecipients = []
+emailRecipient = 'tin@berkeley.edu'
 
 def queryDevicePresent() : 
   # run command /usr/local/bin/deviceQuery  to detect number of live GPU on a system
@@ -139,8 +141,12 @@ def logGpuError(message):
 
 def emailGpuError(message):
   # Emails the error to the recipients in emailRecipients
-  # TODO
+  # Check: node seems to have problem executing mailx.  ++FIXME++
+  os.system('echo "%s" | mailx -s "gpuOffline - %s" %s' % (message,message,emailRecipient) )
   pass
+
+
+############################################################
 
 def main():
   bofhbot_lib.dbg(5, "bofhbot I am")
@@ -153,19 +159,25 @@ def main():
   #print( "host: %s ; deviceQuery found: %s ; gpuExpected: %s ; /dev/nvidia* count: %s"  % (machineName, devQueryFound, gpuExpect, osDevCount ) )
   #print( "host: %s ; gpuExpected: %s ; /dev/nvidia* count: %s ; deviceQuery found: %s"  % (machineName, gpuExpect, osDevCount, devQueryFound ) )    ## // old print by tin
 
+  errorState = False 
+  if( gpuExpect != osDevCount  or   gpuExpect != devQueryFound ):
+    errorState = True
   message =  "host: %s ; gpuExpected: %s ; /dev/nvidia* count: %s ; " \
              "deviceQuery found: %s" \
              % (machineName, gpuExpect, osDevCount, devQueryFound )
-  vprint(1, message)
-
-  if (gpuExpect != osDevCount):
-    vprint(1, "ERROR: expected %s gpu but found %s" % (gpuExpect, osDevCount))
+  if ( errorState ):
+    message = message + " == DISCREPANCY ==" 
     gpuErrorActions(message)
+    vprint(1, message)
     vprint(2, "## checkGpuOnNode.py end (error) ##")
-    exit(1)
-  vprint(2, "## checkGpuOnNode.py end ##")
-  exit(0)
+    exit(1)   # ssh is noisy in this case.  return doesn't set exit code :-\
+  else :
+    vprint(1, message)
+    vprint(2, "## checkGpuOnNode.py end ##")
+    exit(0)
+  #end-if( errorState )
 # main()-end
+
 
 main()
 
