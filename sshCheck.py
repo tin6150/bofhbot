3d1650f22321461ecb1f00cc7d3426fa7c400c32
hchristopher828 <141757936+hchristopher828@users.noreply.github.com>
2023-08-22 15:30:02 -0700

    Update sshCheck.py
    changed to use function from bofh library

--- sshCheck.py
+++ sshCheck.py
@@ -15,37 +15,30 @@ def parseSINFO():
             fields = line.split()
             nodes.add(fields[1])
     return nodes
-
-def isNodeReachable(node):
-    exitCode = os.system(f'/global/home/users/hchristopher/bofhbot/nodeCheck.sh {node}')
-    if(exitCode == 0):
-        return True
-    else:
-        return False
     
 ############################################################
 
 def main():
     nodeList = parseSINFO()
     for n in nodeList:
-        if(isNodeReachable(n)):
+        if(checkSsh(n) == 'up'):
             with open("/global/home/users/hchristopher/bofhbot/data/reachableNodes.txt", "a+") as file_object:
                 file_object.seek(0)
                 # If file is not empty then append '\n
                 data = file_object.read(100)
-                if len(data) > 0 :
-                    file_object.write("\n")
+                #if len(data) > 0 :
+                    #file_object.write("\n")
                 # Append text at the end of file
-                file_object.write(n)
+                file_object.write(n + ',')
         else:
             with open("/global/home/users/hchristopher/bofhbot/data/unreachableNodes.txt", "a+") as file_object:
                 file_object.seek(0)
                 # If file is not empty then append '\n
                 data = file_object.read(100)
-                if len(data) > 0 :
-                    file_object.write("\n")
+                #if len(data) > 0 :
+                    #file_object.write("\n")
                 # Append text at the end of file
-                file_object.write(n)
+                file_object.write(n + ',')
                 
 ############################################################
 
