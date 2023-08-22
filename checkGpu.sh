e40a2fc7841d3401ab28f7845ab2bde2556ce58d
hchristopher828 <141757936+hchristopher828@users.noreply.github.com>
2023-08-22 14:00:40 -0700

    Delete checkGpu.sh

--- checkGpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-> /global/home/users/hchristopher/errorEmail.txt
-sbatch ~/batch_scripts/A40check.sh
-wait
-sbatch ~/batch_scripts/V100check.sh
-wait
-sbatch ~/batch_scripts/2080TIcheck.sh
-wait
-python ~/bofhbot/emailErrorBot.py
